59c79060d55198d551eb18d89d7bfa193bbd522f
Andrew Kelley <superjoe30@gmail.com>
2013-01-12 22:47:39 -0500

    use external map structure

--- index.js
+++ index.js
@@ -1,69 +1,11 @@
 var assert = require('assert')
   , StringSet = require('Set')
   , Heap = require('heap')
+// once https://github.com/domenic/dict/pull/7
+// is pulled we can use that instead of this fork
+  , dict = require('dict_')
 
 module.exports = aStar;
-
-
-/**
- * A Map datastructure. Provides size() and isEmpty() among other methods.
- *
- * NOTE: this class is broken if anyone adds methods to Object.prototype
- *
- * Examples:
- * var map = new Map({"a": 1, "b": 2});
- * map.get("a"); // 1
- * map.put("a", 3);
- * map.get("a"); // 3
- * map.remove("b");
- * map.get("b"); // undefined
- * map.size(); // 1
- * map.isEmpty(); // false
- * map.clear();
- * map.size(); // 0
- * map.isEmpty(); // true
- */
-function Map(items) {
-    this.clear();
-    if (items !== undefined) {
-        for (var key in items) {
-            this.put(key, items[key]);
-        }
-    }
-}
-Map.prototype.put = function(key, value) {
-    this._size = undefined;
-    this.values[key] = value;
-};
-Map.prototype.get = function(key) {
-    return this.values[key];
-};
-Map.prototype.remove = function(key) {
-    delete this.values[key];
-};
-Map.prototype.contains = function(key) {
-    return this.values[key] !== undefined;
-};
-Map.prototype.size = function() {
-    if (this._size === undefined) {
-        this._size = 0;
-        for (var key in this.values) {
-            this._size++;
-        }
-    }
-    return this._size;
-};
-Map.prototype.isEmpty = function() {
-    // don't need to use size
-    for (var key in this.values) {
-        return false;
-    }
-    return true;
-};
-Map.prototype.clear = function() {
-    this.values = {};
-    this._size = 0;
-};
 
 function aStar(params) {
   assert.ok(params.start !== undefined);
@@ -84,14 +26,14 @@ function aStar(params) {
   // leave .parent undefined
   var closed_data_set = new StringSet();
   var open_heap = new Heap(heapComparator);
-  var open_data_map = new Map();
+  var open_data_map = dict();
   open_heap.push(start_node);
-  open_data_map.put(start_node.data, start_node);
+  open_data_map.set(hash(start_node.data), start_node);
   var start_time = new Date();
-  while (!open_data_map.isEmpty()) {
+  while (open_data_map.size) {
     if (new Date() - start_time > params.timeout) break;
     var node = open_heap.pop();
-    open_data_map.remove(node.data);
+    open_data_map.delete(hash(node.data));
     if (params.isEnd(node.data)) {
       // done
       return reconstruct_path(node);
@@ -106,15 +48,15 @@ function aStar(params) {
         continue;
       }
       var g_from_this_node = node.g + params.distance(node.data, neighbor_data);
-      var neighbor_node = open_data_map.get(neighbor_data);
+      var neighbor_node = open_data_map.get(hash(neighbor_data));
       var update = false;
       if (neighbor_node === undefined) {
         // add neighbor to the open set
         neighbor_node = {
-          "data": neighbor_data,
+          data: neighbor_data,
         };
         // other properties will be set later
-        open_data_map.put(neighbor_data, neighbor_node);
+        open_data_map.set(hash(neighbor_data), neighbor_node);
       } else {
         if (neighbor_node.g < g_from_this_node) {
           // skip this one because another route is faster
