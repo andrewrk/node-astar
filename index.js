8a7cbcd418e37c075007710f67c62f47a639ee34
Andrew Kelley <superjoe30@gmail.com>
2013-01-12 21:45:49 -0500

    depend on external heap module

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-var assert = require('assert');
-//var Heap = require('heap').Heap;
-var Set = require('Set');
-
+var assert = require('assert')
+  , StringSet = require('Set')
+  , Heap = require('heap')
 
 module.exports = aStar;
 
@@ -65,92 +64,6 @@ Map.prototype.clear = function() {
     this.values = {};
     this._size = 0;
 };
-/**
- * this class was mostly copied from python's heapq.py module
- */
-function Heap(key_func) {
-    if (key_func === undefined) {
-        key_func = function(x) { return x;};
-    }
-    this.key_func = key_func;
-    this.clear();
-}
-Heap.prototype.clear = function() {
-    this.entries = [];
-};
-Heap.prototype.size = function() {
-    return this.entries.length;
-}
-Heap.prototype.isEmpty = function() {
-    return this.entries.length === 0;
-}
-Heap.prototype.add = function(item) {
-    var key = this.key_func(item);
-    var entry = [key, item];
-    this.entries.push(entry);
-    this._siftDown(0, this.entries.length - 1);
-};
-Heap.prototype.take = function() {
-    var last_entry = this.entries.pop();
-    if (this.entries.length === 0) {
-        // taking from empty returns undefined
-        return last_entry !== undefined ? last_entry[1] : undefined;
-    }
-    var first_entry = this.entries[0];
-    this.entries[0] = last_entry;
-    this._siftUp(0);
-    return first_entry[1];
-};
-Heap.prototype.reheapify = function() {
-    var i;
-    for (i = 0; i < this.entries.length; i++) {
-        this.entries[i][0] = this.key_func(this.entries[i][1]);
-    }
-    for (i = this.entries.length >> 1; i >= 0; i--) {
-        this._siftUp(i);
-    }
-};
-
-Heap.prototype._siftDown = function(start, index) {
-    var new_entry = this.entries[index];
-    while (index > start) {
-        var parent_index = (index - 1) >> 1;
-        var parent_entry = this.entries[parent_index];
-        if (new_entry[0] < parent_entry[0]) {
-            this.entries[index] = parent_entry;
-            index = parent_index;
-            continue;
-        }
-        break;
-    }
-    this.entries[index] = new_entry;
-};
-
-Heap.prototype._siftUp = function(index) {
-    var end_index = this.entries.length;
-    var start_index = index;
-    var new_entry = this.entries[index];
-
-    // start child index with the left child
-    var child_index = 2 * index + 1;
-    while (child_index < end_index) {
-        var right_index = child_index + 1;
-        if (right_index < end_index && this.entries[child_index][0] >= this.entries[right_index][0]) {
-            // right child is smaller
-            child_index = right_index;
-        }
-        // child index is now the smaller child
-        this.entries[index] = this.entries[child_index];
-        index = child_index;
-        child_index = 2 * index + 1;
-    }
-    this.entries[index] = new_entry;
-    this._siftDown(start_index, index);
-};
-
-function defaultHash(node) {
-  return node.toString();
-}
 
 function aStar(params) {
   assert.ok(params.start !== undefined);
@@ -169,15 +82,15 @@ function aStar(params) {
   };
   start_node.f = start_node.h;
   // leave .parent undefined
-  var closed_data_set = new Set();
-  var open_heap = new Heap(function(node) { return node.f; });
+  var closed_data_set = new StringSet();
+  var open_heap = new Heap(heapComparator);
   var open_data_map = new Map();
-  open_heap.add(start_node);
+  open_heap.push(start_node);
   open_data_map.put(start_node.data, start_node);
   var start_time = new Date();
   while (!open_data_map.isEmpty()) {
     if (new Date() - start_time > params.timeout) break;
-    var node = open_heap.take();
+    var node = open_heap.pop();
     open_data_map.remove(node.data);
     if (params.isEnd(node.data)) {
       // done
@@ -216,9 +129,9 @@ function aStar(params) {
       neighbor_node.h = params.heuristic(neighbor_data);
       neighbor_node.f = g_from_this_node + neighbor_node.h;
       if (update) {
-        open_heap.reheapify();
+        open_heap.heapify();
       } else {
-        open_heap.add(neighbor_node);
+        open_heap.push(neighbor_node);
       }
     }
   }
@@ -237,4 +150,12 @@ function reconstruct_path(node) {
     // this is the starting node
     return [node.data];
   }
+}
+
+function defaultHash(node) {
+  return node.toString();
+}
+
+function heapComparator(a, b) {
+  return a.f - b.f;
 }
